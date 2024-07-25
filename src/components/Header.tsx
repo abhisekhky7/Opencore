6e6fbd4810776185b298970a0a859c55479fbb97
abhisekhky7 <anishky07@gmail.com>
2024-07-26 00:36:01 +0530

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,11 +9,11 @@ export default function Header() {
 
 <div className="flex fixed  mynav p-1 border border-gray-400  items-center   ">
     <div className="">
-        <Image src="group.svg" alt="group" width={100} height={37.23} className="p-2 size-14 sm:p3 sm:size-24  m-1 "/>
+        <Image src="group.svg" alt="group" width={100} height={37.23} className="p-2 size-14 sm:p3 sm:size-20  m-1 "/>
     </div>
     <div className="absolute left-1/2 h-full w-px bg-neutral-300 -translate-x-1/2 " style={{opacity:1}}>  </div>
     <div>
-        <Image src="menu.svg" alt="group" width={41} height={36} className="  p-2 size-14 sm:size-24 m-1"/>
+        <Image src="menu.svg" alt="group" width={41} height={36} className="  p-2 size-14 sm:size-20 m-1"/>
     </div>
 
 </div>
